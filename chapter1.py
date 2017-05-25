bca870d82c30c16291e77963be96a6b8cf15c3ff
LinWeiLong <415233062@qq.com>
2017-05-26 00:37:35 +0800

    add file

---
+++ chapter1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @Author: Lucifer
+# @Date:   2016-11-28 23:40:52
+# @Last Modified by:   Lucifer
+# @Last Modified time: 2016-12-12 00:13:05
+
+def print1():
+    # 普通输出
+    print('hello world');
+    # 使用逗号隔开输入的内容
+    print('hello', 'world');
+    # 打印整数 和 打印表达式
+    print(100 + 200);
+    # 输入
+    name = input('input your name: ');
+    print('hello', name);
+print1();
+
+
